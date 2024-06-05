b4772d9c393bd0a2c3610d0ab74068c0cc209e3a
NestorPerez <nolvera@expediagroup.com>
2024-06-04 18:37:39 -0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Nestor Pérez on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
